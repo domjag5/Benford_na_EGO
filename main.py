828ecb06fe149c5b7d0ec8596b8287339b58dda4
Dominik Jagosz <domjag5@st.amu.edu.pl>
2022-05-14 14:51:42 +0200

    lepszy csv

---
+++ main.py
@@ -0,0 +1,7 @@
+from pobieracz_plikow import pobieracz_plikow
+from sprawdzarka import sprawdzarka
+from zbieracz_statystyk import zbieracz_statystyk
+
+pobieracz_plikow()
+zbieracz_statystyk()
+sprawdzarka()
